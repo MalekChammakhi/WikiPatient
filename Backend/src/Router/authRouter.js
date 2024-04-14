d0edd33d95bb028d735c7d0a17aed9b6425d2d3d
Malek Chammakhi <malek.chammakhi@etudiant-fst.utm.tn>
2024-04-15 00:29:15 +0200

    first commit

---
+++ Backend/src/Router/authRouter.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { signup, login ,SendOTP,ChangeForgotPassword,is_verified,updateUser} from '../api/authController.js';
+import upload from '../Middleware/multerConfig.js'
+//S
+
+const authRouter = express.Router();
+
+authRouter.post('/signup', upload, signup);
+authRouter.post('/login', login);
+authRouter.post('/SendOTP', SendOTP);
+authRouter.put('/ChangeForgotPassword', ChangeForgotPassword);
+authRouter.put('/is_verified', is_verified);
+authRouter.put('/updateUser', updateUser);
+
+
+
+
+ChangeForgotPassword
+
+export default authRouter;
